8942c26273551e3c7bde7130e1d137bb82fb3615
ikerismak <ikerismak@gmail.com>
2023-10-03 22:38:52 -0600

    correccion y manejo de errores

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,38 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = "https://api.github.com";
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector(".name");
+const $b = document.querySelector(".blog");
+const $l = document.querySelector(".location");
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+//necesitas async para usar await
+async function displayUser(username) {
+  $n.textContent = "cargando...";
+
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    let data = await response.json();
+
+    // console.log(data)
+
+    $n.textContent = data.name;
+
+    console.log($n);
+
+    $b.textContent = data.blog;
+    $l.textContent = data.location;
+  } catch (error) {
+    console.log("Error trying to get data");
+  }
+
+  //Recibir la informacion en formato json
+  // console.log(data);
+  // console.log(data.name);
 }
 
 function handleError(err) {
-  console.log('OH NO!');
+  console.log("OH NO!");
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser("stolinski").catch(handleError);
